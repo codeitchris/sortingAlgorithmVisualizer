e63b7621194cb5487a26357cca0218c801a7e655
christopher Sebastian <christophersebastian@christophersmbp.myfiosgateway.com>
2022-07-29 22:22:50 -0400

    First commit
    Defining what functions I am going t be using. Installed raylib. Basic set up for the work to begin tomorrow

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "raylib.h"
+
+
+// n^2 complexity
+void bubblesort(){
+
+}
+
+void selectionsort(){
+
+}
+
+void insertionsort(){
+
+}
+
+//nlogn
+
+void mergesort(){
+
+}
+
+void quicksort(){
+
+}
+
+void heapsort(){
+
+}
+
+//hybrid
+
+void timsort(){
+
+}
+
+void introsort(){
+
+}
+
+
+
+int main() {
+    
+    return 0;
+}
