5a7505c85884abba2dc1e39b9e0e2de0ea8a213d
christopher Sebastian <christophersebastian@christophersmbp.myfiosgateway.com>
2022-08-04 18:45:31 -0400

    big update
    added functionality for multiple files and added selection sort

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
+#include <utility>
 
 #define ScreenWidth 1200
 #define ScreenHeight 800
@@ -12,10 +13,12 @@
 
 #define FPS 120
 
-
+#define LOCALMINUM 4
+#define POSITIONVAL 3
 #define SELECTED 2 
 #define SORTED 1
 #define NORMAL 0
+
 
 
 int numOfColumn = 50;
@@ -26,6 +29,12 @@ std::vector<std::pair<int, int>     > arr(numOfColumn);
 
 Color findColor(int pillerState) {
     switch (pillerState) {
+        case LOCALMINUM:
+            return RED;
+            break;
+        case POSITIONVAL:
+            return DARKGRAY;
+            break;
         case SELECTED:
             return LIGHTGRAY;
             break;
@@ -38,14 +47,17 @@ Color findColor(int pillerState) {
     }
     }
     
-void Bubble_Sort_Button(float size, char Bubble_Sort_text[]);
-void menue();
+void openerbutton(float size, char Bubble_Sort_text[]);
+void opener();
 void button(float x, float y, char *Text, Color color, bool &state);
-void Start_Button(float size, float font, char Start[]);
 void bubblesort(std::vector<std::pair<int, int> > &arr);
-void sortarray();
-void ChangeSpeed(char operation, int &value);
-void showEndingScreen();
+void whichSort();
+void speedFunction(char operation, int &value);
+void endScreen();
+void nSquaredSortOptions();
+void nLogNsortOptions();
+void hybridSortOptions();
+void complexityChoice();
 
 
 
@@ -61,44 +73,53 @@ void drawarr(std::vector<std::pair<int, int> > arr) {
 }
 
 bool shouldRND = true;
-bool shldmenue = true;
-bool BubbleSortPressed = false;
-bool addSpeed = false;
-bool subSpeed = false;
+bool shldmenue = false;
+bool shldopener = true;
+bool speedPlus = false;
+bool speedDec = false;
 bool addSize = false;
-bool normalSize = false;
-bool normalSpeed = false;
-bool shouldShowStartOptions = false;
-bool shouldStartGame = false;
-bool shouldStartSorting = false;
-bool NormalSize = false;
-bool NormalSpeed= false;
+bool normSize = false;
+bool normSpeed = false;
+bool doStartOptions = false;
+bool shldSort = false;
 bool subSize = false;
-bool shouldShowMenuScreen = false;
-bool shouldShowEndingScreen = false;
+bool doMenue = false;
+bool doEnding = false;
+bool bubbleSortbool = false;
+bool selectionSortbool = false;
+bool insertionSortbool = false;
+bool mergeSortbool = false;
+bool quickSortbool = false;
+bool heapSortbool = false;
+bool timSortbool = false;
+bool introSortbool = false;
+bool nsquaredsortoptionbool = false;
+bool nlognsortotionbool = false;
+bool hybridbool = false; 
+bool openerButtonPressed = false;
 
 
 
 
 
-void menue(){
+void opener(){
     float font = (2.5 * GetScreenWidth())/100;
-    char Bubble_Sort_Text[] = "Bubble Sort!?";
-    float tmp = MeasureText(Bubble_Sort_Text, font)/2;
-    Bubble_Sort_Button(tmp, Bubble_Sort_Text);
+    char opening_text[] = "You want some sorting!?";
+    float tmp = MeasureText(opening_text, font)/2;
+    openerbutton(tmp, opening_text);
     
     
     drawarr(arr);
 }
-void Bubble_Sort_Button(float size, char Bubble_Sort_text[]){
+void openerbutton(float size, char Bubble_Sort_text[]){
     Color color;
-    if (BubbleSortPressed)
+    if (openerButtonPressed)
         color = DARKBROWN;
     else
         color = GREEN; 
 
 
-    button(GetScreenWidth()/2- size, GetScreenHeight()/20, Bubble_Sort_text, color, BubbleSortPressed);
+    button(GetScreenWidth()/2- size, GetScreenHeight()/20, Bubble_Sort_text, color, openerButtonPressed);
 }
 
 void button(float x, float y, char *Text, Color color, bool &state){
@@ -142,7 +163,7 @@ void RNDarr(std::vector<std::pair<int, int> >& arr) {
 
 
 
-void ChangeSpeed(char operation, int &value) {
+void speedFunction(char operation, int &value) {
     switch(operation)
     {
         case '-':
@@ -195,57 +216,62 @@ void ChangeSize(char operation, int &value) {
 }
 
 void showStartOptions() {
+
     float font = (2.5*GetScreenWidth())/100;
 
     char StartText[] = "start sorting";
     float tmp=(27*GetScreenWidth())/100;
-    Start_Button(tmp, font, StartText);
+    button(tmp, GetScreenHeight()/20 + font*2, StartText, DARKGRAY, shldSort);
+    if(shldSort){
+        return;
+    }
+        
 
     tmp+= MeasureText(StartText, font) + 75;
     char RandomizeArrayText[] = "randomize array";
     button(tmp, GetScreenHeight()/20 + font*2, RandomizeArrayText, DARKGRAY, shouldRND);
 
-    addSpeed = false;
-    subSpeed = false;
+    speedPlus = false;
+    speedDec = false;
     addSize = false;
     subSize = false;
 
-    normalSize = false;
-    normalSpeed = false;
+    normSize = false;
+    normSpeed = false;
 
     tmp = (84.2*GetScreenWidth())/100;
     char timeButtonText[] = "speed";
 
-    button(tmp, GetScreenHeight()/20 + font*2, timeButtonText, DARKGRAY, normalSpeed);
+    button(tmp, GetScreenHeight()/20 + font*2, timeButtonText, DARKGRAY, normSpeed);
 
-    if(normalSpeed){
-        ChangeSpeed('/', SortingSpeed);
+    if(normSpeed){
+        speedFunction('/', SortingSpeed);
         return;
     }
 
     tmp+= MeasureText(timeButtonText, font) + 20;
     char SpeedPlisButtonText[] = "+";
-    button(tmp, GetScreenHeight()/20 + font*2, SpeedPlisButtonText, ORANGE, addSpeed);
+    button(tmp, GetScreenHeight()/20 + font*2, SpeedPlisButtonText, ORANGE, speedPlus);
 
-    if(addSpeed) {
-        ChangeSpeed('+', SortingSpeed);
+    if(speedPlus) {
+        speedFunction('+', SortingSpeed);
         return;
     }
 
     tmp += MeasureText(SpeedPlisButtonText, font) +  20;
     char SpeedMinuesButtonText[] = "-";
-    button(tmp, GetScreenHeight()/20 + font*2, SpeedMinuesButtonText, ORANGE, subSpeed);
+    button(tmp, GetScreenHeight()/20 + font*2, SpeedMinuesButtonText, ORANGE, speedDec);
 
-    if(subSpeed) {
-        ChangeSpeed('-', SortingSpeed);
+    if(speedDec) {
+        speedFunction('-', SortingSpeed);
         return;
     }
 
     tmp = (5*GetScreenWidth())/100;
     char SizeButtonText[] = "size";
-    button(tmp, GetScreenHeight()/20 + font*2, SizeButtonText, DARKGRAY, normalSize);
+    button(tmp, GetScreenHeight()/20 + font*2, SizeButtonText, DARKGRAY, normSize);
 
-    if(NormalSize) {
+    if(normSize) {
         ChangeSize('/', numOfColumn);
         return;
     }
@@ -267,21 +293,14 @@ void showStartOptions() {
         ChangeSize('-', numOfColumn);
         return;
     }
+
 }
 
 
-void Start_Button(float size, float font, char Start[]) {
-    button(size, GetScreenHeight()/20 + font*2, Start, DARKGRAY, shouldStartSorting);
-
-    return;
-}
 
 
-void sortarray() {
-    bubblesort(arr);
 
-    drawarr(arr);
-}
+
 
 // n^2 complexity
 
@@ -295,7 +314,7 @@ void bubblesort(std::vector<std::pair<int, int> > &arr){
             arr[i].second = SELECTED;
 
             if (arr[i] > arr[i+1]){
-                std::swap(arr[i], arr[i+1]);
+                std::swap(arr[i].first, arr[i+1].first);
 
                 swapped = true;
             }
@@ -322,48 +341,262 @@ void bubblesort(std::vector<std::pair<int, int> > &arr){
         arr[i].second = SORTED;
     }
 }
+void selectionsort(std::vector<std::pair<int, int> > &arr){
+    int localmin = 0;
+    int oldnews = 0;
+    int endpt = numOfColumn;
+    bool swapped;
+    do {
+    for(int i = 0; i < endpt-1; i++){
+        swapped = false;
+        localmin = i+1;
+        arr[i].second = POSITIONVAL;
+        for(int j = i + 1; j < endpt-1; j++){
+            arr[j].second = SELECTED;
+            if (arr[j].first < arr[i].first){
+                if(arr[j].first < arr[localmin].first ){
+                    oldnews = localmin;
+                    localmin = j;
+                    arr[localmin].second = LOCALMINUM;
+                    arr[oldnews].second = SELECTED;
+                }
+            }
+        }
+        if(arr[localmin].first < arr[i].first){
+            std::swap(arr[i], arr[localmin]);
+            swapped = true;
+        }  
+            BeginDrawing();
 
+            ClearBackground(BLUE);
 
-void showEndingScreen() {
-    drawarr(arr);
+            for(int k = numOfColumn - 1; k>= endpt; k--)
+                arr[k].second = SORTED;
+            
+            drawarr(arr);
 
-    shouldShowMenuScreen = true;
-    shouldShowStartOptions = true;
-    
+            for(int k = i; k>= 0; k--)
+                arr[k].second = NORMAL;
+            EndDrawing();
+    } 
+    } while(swapped);
+
+    for (int i = numOfColumn - 1; i>= 0; i--){
+        arr[i].second = SORTED;
+    }
 }
 
-void selectionsort(){
-
-}
-
-void insertionsort(){
+void insertionsort(std::vector<std::pair<int, int> > &arr){
 
 }
 
 //nlogn
 
-void mergesort(){
+void mergesort(std::vector<std::pair<int, int> > &arr){
 
 }
 
-void quicksort(){
+void quicksort(std::vector<std::pair<int, int> > &arr){
 
 }
 
-void heapsort(){
+void heapsort(std::vector<std::pair<int, int> > &arr){
 
 }
 
 //hybrid
 
-void timsort(){
+void timsort(std::vector<std::pair<int, int> > &arr){
 
 }
 
-void introsort(){
+void introsort(std::vector<std::pair<int, int> > &arr){
 
 }
 
+void whichSort(int x, int y) {
+    if(x == 0){
+        if(y == 0){
+            bubblesort(arr);
+            drawarr(arr);
+        }
+        if (y== 1){
+            selectionsort(arr);
+            drawarr(arr);
+        }
+        if ( y== 2){
+            insertionsort(arr);
+            drawarr(arr);
+        }
+    }
+
+    if(x == 1){
+        if(y == 0){
+            mergesort(arr);
+            drawarr(arr);
+        }
+        if (y== 1){
+            quicksort(arr);
+            drawarr(arr);
+        }
+        if ( y== 2){
+            heapsort(arr);
+            drawarr(arr);
+        }
+    }
+
+    if(x == 2){
+        if(y == 0){
+            timsort(arr);
+            drawarr(arr);
+        }
+        if (y== 1){
+            introsort(arr);
+            drawarr(arr);
+        }
+    }
+    
+
+    
+}
+
+
+void endScreen() {
+    drawarr(arr);
+
+    doMenue = true;
+    doStartOptions = true;
+    
+}
+
+int typeSortcounter = 0;
+
+
+bool nSquaredComplexity = false;
+bool nlognComplexity = false;
+bool hybridcomplexity = false;
+
+
+
+
+void complexityChoice() {
+    float font = (2.5*GetScreenWidth())/100;
+
+    char nSquaredText[] = "n-Squared";
+    float tmp=(27.0*GetScreenWidth())/100;
+    button(tmp, GetScreenHeight()/20 + font*2, nSquaredText, DARKGRAY, nSquaredComplexity);
+
+    if(nSquaredComplexity){
+        typeSortcounter = 0;
+        return;
+    }
+
+    tmp+= MeasureText(nSquaredText, font) + 100;
+    char nlogntext[] = "nLog(n)";
+    button(tmp, GetScreenHeight()/20 + font*2, nlogntext, DARKGRAY, nlognComplexity);
+
+    if(nlognComplexity){
+        typeSortcounter = 1;
+        return;
+    }
+
+    tmp+= MeasureText(nlogntext, font) + 100;
+    char hybridtext[] = "hybrid";
+    button(tmp, GetScreenHeight()/20 + font*2, hybridtext, DARKGRAY, hybridcomplexity);
+
+    if(hybridcomplexity){
+        typeSortcounter = 2;
+        return;
+    }
+}
+
+
+int sortCount = 0;
+
+
+void nSquaredSortOptions() {
+    float font = (2.5*GetScreenWidth())/100;
+    float tmp = (15.0*GetScreenWidth())/100;
+
+
+    char bubblesortText[] = "bubble sort";
+    button(tmp, GetScreenHeight()/20 + font*2, bubblesortText, DARKGRAY, bubbleSortbool);
+    if(bubbleSortbool){
+        sortCount = 0;
+        return;
+    }
+    
+
+    tmp += MeasureText(bubblesortText, font) + 80;
+    char selectionSortText[] = "selection sort";
+    
+    button(tmp, GetScreenHeight()/20 + font*2, selectionSortText, DARKGRAY, selectionSortbool);
+    if(selectionSortbool){
+        sortCount = 1;
+        return;
+    }
+    
+    tmp+= MeasureText(selectionSortText, font) + 80;
+    char insertionSortText[] = "insertion sort";
+    button(tmp, GetScreenHeight()/20 + font*2, insertionSortText, DARKGRAY, insertionSortbool);
+    if(insertionSortbool){
+        sortCount = 2;
+        return;
+    }
+
+}
+
+void nLogNsortOptions(){
+    float font = (2.5*GetScreenWidth())/100;
+    float tmp=(20.0*GetScreenWidth())/100;
+    
+    char mergeSortText[] = "merge sort";
+    button(tmp, GetScreenHeight()/20 + font*2, mergeSortText, DARKGRAY, mergeSortbool);
+    if(mergeSortbool){
+        sortCount = 0;
+        return;
+    }
+
+
+    tmp+= MeasureText(mergeSortText, font) + 80;
+    char quickSortText[] = "quick sort";
+    button(tmp, GetScreenHeight()/20 + font*2, quickSortText, DARKGRAY, quickSortbool);
+    if(selectionSortbool){
+        sortCount = 1;
+        return;
+    }
+
+
+    tmp += MeasureText(quickSortText, font) +  80;
+    char heapSortText[] = "heap sort";
+    button(tmp, GetScreenHeight()/20 + font*2, heapSortText, DARKGRAY, heapSortbool);
+    if(heapSortbool){
+        sortCount = 2;
+        return;
+    }
+    
+}
+ 
+void hybridSortOptions(){
+    float font = (2.5*GetScreenWidth())/100;
+    float tmp=(15.0*GetScreenWidth())/100;
+
+    char timSortText[] = "timsort";
+    button(tmp, GetScreenHeight()/20 + font*2, timSortText, DARKGRAY, timSortbool);
+    if(timSortbool){
+        sortCount = 0;
+        return;
+    }
+
+    tmp += MeasureText(timSortText, font) + 80;
+    char introSortText[] = "introsort";
+    button(tmp, GetScreenHeight()/20 + font*2, introSortText, DARKGRAY, introSortbool);
+    if(introSortbool){
+        sortCount = 1;
+        return;
+    }
+
+}
 
 
 
@@ -371,7 +604,7 @@ int main() {
     
     
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(ScreenWidth, ScreenHeight, "First sorting test");
+    InitWindow(ScreenWidth, ScreenHeight, "begin");
     SetWindowMinSize(MinWindowWidth, MinWindowHeight);
 
 
@@ -379,35 +612,81 @@ int main() {
 
     while(!WindowShouldClose()){
         
-        if (shldmenue)
-            menue();
+        if (shldopener)
+            opener();
         
-        shouldShowStartOptions = false;
-        if (BubbleSortPressed)
-            shouldShowStartOptions = true;
+        
+        if(openerButtonPressed) 
+            complexityChoice();
+        
+        
+        if(nSquaredComplexity){
+            openerButtonPressed = false;
+            nSquaredSortOptions();
+        } 
+        if(nlognComplexity){
+            openerButtonPressed = false;
+            nLogNsortOptions();
+        } 
+        if(hybridcomplexity){
+            openerButtonPressed = false;
+            hybridSortOptions();
+        }
+        
+        if (bubbleSortbool){
+            nSquaredComplexity = false;
+            doStartOptions = true;
+        }
+        if(insertionSortbool){
+            nSquaredComplexity = false;
+            doStartOptions = true;
+        }
+        if(selectionSortbool){
+            nSquaredComplexity = false;
+            doStartOptions = true;
+        }
+        if(mergeSortbool){
+            nlognComplexity = false;
+            doStartOptions = true;
+        }
+        if(quickSortbool ){
+            nlognComplexity = false;
+            doStartOptions = true;
+        }
+        if(heapSortbool){
+            nlognComplexity = false;
+            doStartOptions = true;
+        }
+        if(timSortbool){
+            hybridcomplexity = false; 
+            doStartOptions = true;
+        }
+        if(introSortbool) {
+            hybridcomplexity = false; 
+            doStartOptions = true;
+        }
 
-
-        if (shouldShowStartOptions)
+        if (doStartOptions)
             showStartOptions();
         
         
         if(shouldRND)
             RNDarr(arr);
         
-        if(shouldStartSorting){
-            shouldShowMenuScreen = false;
-            shouldShowStartOptions = false;
+        if(shldSort){
+            doMenue = false;
+            doStartOptions = false;
 
             SetTargetFPS(SortingSpeed);
-            sortarray();
+            whichSort(typeSortcounter, sortCount);
             
-            shouldStartSorting = false;
-            shouldShowEndingScreen = true;
+            shldSort = false;
+            doEnding = true;
             
         }
         
-        if(shouldShowEndingScreen)
-            showEndingScreen();
+        if(doEnding)
+            endScreen();
         
         BeginDrawing();
 
